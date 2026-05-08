b41ba44b20412dc1432b48a51ee002a510688431
luhdias-png <andre_lucas.94@hotmail.com>
2026-05-08 07:47:57 -0300

    conectado com o backend

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,28 +1,33 @@
-import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } 
-  from "@phosphor-icons/react";
+import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react"
+
 
 function Footer() {
-  let data = new Date().getFullYear();
 
-  return (
-    <>
-      <div className="flex justify-center bg-indigo-900 text-white">
-        <div className="container flex flex-col items-center py-4">
-          <p className='text-xl font-bold'>
-            Blog Pessoal Generation | Copyright: {data}
-          </p>
-          
-          <p className='text-lg'>Acesse nossas redes sociais</p>
-          
-          <div className="flex gap-2">
-            <LinkedinLogoIcon size={48} weight='bold' />
-            <InstagramLogoIcon size={48} weight='bold' />
-            <FacebookLogoIcon size={48} weight='bold' />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    let data = new Date().getFullYear()
+
+    return (
+        <>
+            <div className="flex justify-center bg-indigo-900 text-white">
+                <div className="container flex flex-col items-center py-4">
+                    <p className='text-xl font-bold'>
+                            Blog Pessoal Andre | Copyright: {data}
+                        </p>
+                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <div className='flex gap-2'>
+                        <a href="https://www.linkedin.com/" target="_blank">
+                            <LinkedinLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/" target="_blank">
+                            <InstagramLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.facebook.com/" target="_blank">
+                            <FacebookLogoIcon size={48} weight='bold' />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default Footer;
+export default Footer
