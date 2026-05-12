a1959507955d7de56b408eeabf1a1faf58d11586
luhdias-png <andre_lucas.94@hotmail.com>
2026-05-11 23:18:07 -0300

    feat: adicionado funcao delete.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import { AuthProvider } from './contexts/AuthContext'
+import ListaTemas from './components/tema/listatemas/ListaTemas'
+import FormTema from './components/tema/formtema/FormTema'
+import DeletarTema from './components/tema/deletartema/DeletarTema'
 
 function App() {
 	return (
@@ -14,10 +17,14 @@ function App() {
 				<Navbar />
 				<div className="min-h-[80vh]">
 					<Routes>
-						<Route path="/" element={<Login />} />
-						<Route path="/login" element={<Login />} />
+						<Route path="/" element={<Login />}/>
+						<Route path="/login" element={<Login />}/>
 						<Route path="/home" element={<Home />} />
 						<Route path="/cadastro"	element={<Cadastro />}/>
+						<Route path='/temas' element={<ListaTemas />}/>
+						<Route path='/cadastrartema' element={<FormTema/>} />
+						<Route path='/editartema/:id' element={<FormTema/>} />
+						<Route path='/deletartema/:id' element={<DeletarTema/>} />
 					</Routes>
 				</div>
 				<Footer />
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 
+
 const api = axios.create({
     baseURL: 'https://blogpessoal-nest-atividade-generation.onrender.com/'
 })
@@ -15,3 +16,21 @@ export const login = async (url: string, dados: Object, setDados: Function) =>{
     setDados(resposta.data)
 }
 
+export const buscar = async(url: string, setDados: Function, header: Object) => {
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
+}
+
+export const cadastrar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.post(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const atualizar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.put(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const deletar = async (url: string, header: Object) => {
+    await api.delete(url, header)
+}
